1cdafe8f926ddc02bd21419ea256c2a0f5a90107
AutumnVN <autumnvnchino@gmail.com>
2026-09-05 21:44:58 +0700

    nuke the lua folder

--- _Lua/Game/UI/ActivityTheme/30103/Task/breakouttaskpanel.lua
+++
@@ -1,9 +0,0 @@
-local BreakOutTaskPanel = class("BreakOutTaskPanel", BasePanel)
-BreakOutTaskPanel._sUIResRootPath = "UI_Activity/"
-BreakOutTaskPanel._tbDefine = {
-	{
-		sPrefabPath = "30103/Task.prefab",
-		sCtrlName = "Game.UI.ActivityTheme.30103.Task.BreakOutTaskCtrl"
-	}
-}
-return BreakOutTaskPanel
--- characterid.js
+++ characterid.js
@@ -1,11 +1,15 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { writeFileSync } = require('fs');
 
-const AVGCHARACTER = readFileSync('./_Lua/Game/UI/Avg/_en/Preset/AvgCharacter.lua', 'utf-8');
+const AVGCHARACTER_URL = 'https://github.com/MakoStar/StellaSoraData/raw/refs/heads/main/_Lua/Game/UI/Avg/_en/Preset/AvgCharacter.lua';
 
 const characterId = {};
 
-AVGCHARACTER.matchAll(/id = "avg1_(\d{3})",\r?\n?.{0,10}name = "([^"]+)"/g).forEach(([, id, name]) => {
-    characterId[id] = name;
-});
+fetch(AVGCHARACTER_URL)
+    .then(response => response.text())
+    .then(AVGCHARACTER => {
+        AVGCHARACTER.matchAll(/id = "avg1_(\d{3})",\r?\n?.{0,10}name = "([^"]+)"/g).forEach(([, id, name]) => {
+            characterId[id] = name;
+        });
 
-writeFileSync('characterid.json', JSON.stringify(characterId, null, 4));
+        writeFileSync('characterid.json', JSON.stringify(characterId, null, 4));
+    });
